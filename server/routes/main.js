2d79275cfa4c0f76a73a71386e6a62c88f046f9c
MSI\hosiu <hosiuchuen05@gmail.com>
2023-09-17 19:52:43 -0400

    created MongoDb for posts

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,16 +1,25 @@
-const express = require('express')
+const express = require('express');
 
 const router = express.Router();
+const Post = require('../models/Post');
 
 //Routes
-router.get('', (req, res) => {
+router.get('', async (req, res) => {
     const locals = {
         title: "NodeJS Blog",
         description: "Simple Blog created with NodeJS, Express & MongoDB"
     }
 
-    res.render('index', { locals });
-})
+    try {
+        const data = await Post.find();
+        res.render('index', { locals, data });
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+
+
 
 router.get('/about', (req, res) => {
     res.render('about');
