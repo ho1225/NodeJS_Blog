771ba9b37a14fb0af1d5b63f7150068ef32edd73
MSI\hosiu <hosiuchuen05@gmail.com>
2023-09-21 20:40:26 -0400

    add pagination for post list

--- server/routes/main.js
+++ server/routes/main.js
@@ -5,14 +5,31 @@ const Post = require('../models/Post');
 
 //Routes
 router.get('', async (req, res) => {
-    const locals = {
-        title: "NodeJS Blog",
-        description: "Simple Blog created with NodeJS, Express & MongoDB"
-    }
-
     try {
-        const data = await Post.find();
-        res.render('index', { locals, data });
+        const locals = {
+            title: "NodeJS Blog",
+            description: "Simple Blog created with NodeJS, Express & MongoDB"
+        }
+
+        let perPage = 10;
+        let page = req.query.page || 1; //if no page query after the url, set it 1
+
+        const data = await Post.aggregate([ {$sort: { createdAt: -1 } }])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+
+        const count = await Post.count();
+        const nextPage = parseInt(page) + 1
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        res.render('index', { 
+            locals, 
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null
+        });
     } catch (error) {
         console.log(error)
     }
